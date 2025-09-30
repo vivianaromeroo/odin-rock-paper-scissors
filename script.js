e5c84269512c352b474bd6092e6d8223af66ba72
vivianaromeroo <155496388+vivianaromeroo@users.noreply.github.com>
2025-09-30 18:46:46 -0400

    Replaced console logs with DOM object manipulation

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ const ROCK = "rock"
 const PAPER = "paper"
 const SCISSORS = "scissors"
 
+const rules = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper"
+}
+
+const resultEl = document.querySelector(".result")
+const outcomeEl = document.querySelector(".explanation")
+const numbersEl = document.querySelector(".numbers")
+const roundEl = document.querySelector(".Round")
+
 function getComputerChoice() {
     let num = Math.random()
 
@@ -20,61 +31,52 @@ function getComputerChoice() {
 
 function playRound(humanChoice) {
     let computerChoice = getComputerChoice()
-
-    if (humanChoice == computerChoice) {
-        console.log("Draw!")
-    } else if (humanChoice == ROCK) {
-        if (computerChoice == PAPER) {
-            computerScore += 1
-            console.log("You lose! Paper beats rock.")
-        } else if (computerChoice == SCISSORS) {
-            humanScore += 1
-            console.log("You win! Rock beats scissors.")
-        }
-    } else if (humanChoice == PAPER) {
-        if (computerChoice == ROCK) {
-            humanScore += 1
-            console.log("You win! Paper beats rock.")
-        } else if (computerChoice == SCISSORS) {
-            computerScore += 1
-            console.log("You lose! Scissors beats paper.")
-        }
-    } else if (humanChoice == SCISSORS) {
-        if (computerChoice == ROCK) {
-            computerScore += 1
-            console.log("You lose! Rock beats scissors.")
-        } else if (computerChoice == PAPER) {
-            humanScore += 1
-            console.log("You win! Scissors beats paper.")
-        }
-    }
-    console.log("Current Score: " + humanScore + " - " + computerScore)
+    displayResults(humanChoice, computerChoice)
 
     if (round < 5) {
         round++
+        roundEl.textContent = "Round " + round
     } else {
-        document.querySelectorAll(".button").forEach(button => {
-            button.disabled = true;
-        })
         endGame()
     }
 }
 
-function endGame() {
-    if (humanScore > computerScore) {
-        console.log("You win the game!")
-    } else if (computerScore > humanScore) {
-        console.log("You lose the game!")
+function displayResults(hChoice, cChoice) {
+    if (hChoice === cChoice) {
+        resultEl.textContent = "Draw!"
+        outcomeEl.textContent = ''
+    } else if (rules[hChoice] === cChoice) {
+        humanScore++
+        resultEl.textContent = "You win!"
+        outcomeEl.textContent = `${hChoice} beats ${cChoice}.`
     } else {
-        console.log("Draw game!")
+        computerScore++
+        resultEl.textContent = "You lose!"
+        outcomeEl.textContent = `${cChoice} beats ${hChoice}.`
+    }
+
+    numbersEl.textContent = `${humanScore} - ${computerScore}`
+}
+
+function endGame() {
+    document.querySelectorAll(".button").forEach(button => {
+        button.disabled = true
+    })
+
+    const outcome = document.querySelector(".final")
+
+    if (humanScore > computerScore) {
+        outcome.textContent = "You won the game!"
+    } else if (computerScore > humanScore) {
+        outcome.textContent = "You lost the game!"
+    } else {
+        outcome.textContent = "Draw game!"
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const parentContainer = document.querySelector('.buttons');
-    parentContainer.addEventListener('click', function(event) {
-        const clickedButton = event.target.closest("button");
-        if (!clickedButton) return; // clicked outside a button
-        playRound(clickedButton.id);
-    })
+const parentContainer = document.querySelector('.buttons')
+parentContainer.addEventListener('click', function(event) {
+    const clickedButton = event.target.closest("button")
+    if (!clickedButton) return // clicked outside a button
+    playRound(clickedButton.id)
 })
